f61a2e24ce4d65787be2dab7c618b70271692e8a
Ath1thyan <athitasks@gmail.com>
2024-08-09 07:08:31 +0530

    created signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const signup = async (req, res, next) => {
 
@@ -25,5 +29,28 @@ export const signup = async (req, res, next) => {
     catch (error) {
         next(error)
     }
-    
 };
+
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, "User not found."));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(401, "Invalid user credentials."));
+        }
+        // Create and sign JWT token
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: hashedPassword, ...restItems } = validUser._doc;
+        // Set cookie with JWT token
+        const expiryDuration = new Date(Date.now() + 3600000);
+        res.cookie('access_token', token, {httpOnly: true, expires: expiryDuration}).status(200).json(restItems);
+    }
+    catch (error) {
+        next(error)
+    }
+}
